6715e7cc3147c725e7920b138f21b178a21b3aaa
Mark Alvares <mark.alvarez2@gmail.com>
2025-06-25 15:01:33 +0100

    getting a slightly better looking result

--- mixed_stokes.py
+++ mixed_stokes.py
@@ -3,62 +3,88 @@ import matplotlib.pyplot as plt
 from firedrake import *
 from firedrake.pyplot import tripcolor
 from firedrake.petsc import PETSc
+import os
 
-# Define the mesh and function spaces
-mesh = UnitSquareMesh(60, 60)
+def plot_and_save(u_: Function, p_: Function, filename: str) -> None:
+        fig, ax = plt.subplots(2, figsize = (10,10))
+        quiver(u_, axes = ax[0])
+        #colors = tripcolor(u, axes = ax[0])
+        #fig.colorbar(colors)
+        ax[0].set_title("Velocity field")
 
-V = VectorFunctionSpace(mesh, "CG", 2)
-W = FunctionSpace(mesh, "CG", 1)
-Z = V * W
+        colors = tripcolor(p_, axes = ax[1])
+        fig.colorbar(colors)
+        ax[1].set_title("Pressure field")
 
-# Define trial (solution) and test functions
-u,p = TrialFunctions(Z)
-v,q = TestFunctions(Z)
+        plt.savefig(filename)
 
-# Define the RHS of the PDEs. We will add the two equations together to get our governing variational equation.
-x,y = SpatialCoordinate(mesh)
+def define_and_solve(N: int) -> None:
+    print(f"{N=}")
 
-lapl_u = as_vector([
-    2000*(-1 + x**2)*(y**2*(3 - 8*y + 5*y**2) - 10*x*y**2*(3 - 8*y + 5*y**2) - 6*x**3*(1 - 8*y + 10*y**2) + 3*x**4*(1 - 8*y + 10*y**2) + 3*x**2*(1 - 8*y + 25*y**2 - 40*y**3 + 25*y**4)),
-    -4000*(-1 + x)*y*(6*(-1 + y)**2*y**2 - 7*x**3*(3 - 12*y + 10*y**2) + x**4*(9 - 36*y + 30*y**2) + x*(-3 + 12*y - 40*y**2 + 60*y**3 - 30*y**4) + 5*x**2*(3 - 12*y + 16*y**2 - 12*y**3 + 6*y**4))
-])
+    # Define the mesh and function spaces
+    mesh = UnitSquareMesh(N, N)
+    x,y = SpatialCoordinate(mesh)
 
-# grad_p = as_vector([
-#     pi**2*(y**3*cos(2*pi*x**2*y) - 2*x*y*(pi*x*y**2*cos(2*pi*x*y**2) + 2*pi*x*y**3*sin(2*pi*x**2*y) + sin(2*pi*x*y**2))),
-#     -pi**2*x*(-3*y**2*cos(2*pi*x**2*y) + x*(4*pi*x*y**2*cos(2*pi*x*y**2) + 2*pi*x*y**3*sin(2*pi*x**2*y) + sin(2*pi*x*y**2)))
-# ])
+    V = VectorFunctionSpace(mesh, "CG", 2)
+    Q = FunctionSpace(mesh, "CG", 1)
+    Z = V * Q
 
-# try constant pressure?
-f = Function(V).interpolate(
-    -lapl_u + as_vector([0,0])# grad_p
-)
+    # Define the true solutions
+    u_true = Function(V).interpolate(as_vector([
+        1000*x**2*(1-x)**4*y**2*(1-y)*(3-5*y),
+        -2000*x*(1-x)**3*(1-3*x)*y**3*(1-y)**2
+    ]))
 
-a = (inner(grad(u), grad(v)) - inner(p, div(v)) - inner(q, div(u)))*dx
-L = inner(f, v)*dx
+    p_true = Function(Q).interpolate(
+        pi**2*(x*y**3*cos(2*pi*x**2*y) - x**2*y*sin(2*pi*x*y)) + 1/8
+    )
 
-# the BC on the velocity space (first subspace of Z) is v = (0,0) on all boundaries
-bcs = [DirichletBC(Z.sub(0), Constant((0, 0)), (1, 2, 3, 4))]
+    # Define trial (solution) and test functions
+    u,p = TrialFunctions(Z)
+    v,q = TestFunctions(Z)
 
-up = Function(Z)
+    # Define the RHS of the PDEs. We will add the two equations together to get our governing variational equation.
+    lapl_u = as_vector([
+        -2000*(-1 + x)**2 * (x**3 * (6 - 60*y**2) + y**2 * (-3 + 5*y**2) + x**4 * (-3 + 30*y**2) + x * (30*y**2 - 50*y**4) + 3*x**2 * (-1 - 5*y**2 + 25*y**4)),
+        4000*(-1 + x)*y*(x**3 * (21 - 70*y**2) + 6*y**2 * (-1 + y**2) + x**4 * (-9 + 30*y**2) + x*(3 + 20*y**2 - 30*y**4) + 5*x**2 * (-3 + 4*y**2 + 6*y**4))
+    ])
 
-# this was something to do with uniqueness of p. Maybe I don't need?
-#nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True)])
+    grad_p = as_vector([
+        pi**2*(y**3*cos(2*pi*x**2*y) - 2*x*y*(pi*x*y**2*cos(2*pi*x*y**2) + 2*pi*x*y**3*sin(2*pi*x**2*y) + sin(2*pi*x*y**2))),
+        -pi**2*x*(-3*y**2*cos(2*pi*x**2*y) + x*(4*pi*x*y**2*cos(2*pi*x*y**2) + 2*pi*x*y**3*sin(2*pi*x**2*y) + sin(2*pi*x*y**2)))
+    ])
 
-# Solve the system
-solve(a == L, up, bcs=bcs)#, nullspace=nullspace,
-    # solver_parameters={"ksp_type": "gmres",
-    #                     "mat_type": "aij",
-    #                     "pc_type": "lu",
-    #                     "pc_factor_mat_solver_type": "mumps"})
-u, p = up.subfunctions
+    f = Function(V).interpolate(
+        -lapl_u + grad_p
+    )
 
-# Plot the results
-fig, ax = plt.subplots(2, figsize = (10,10))
-quiver(u, axes = ax[0])
-ax[0].set_title("Velocity field")
+    # Define bilinear forms
+    a = (inner(grad(u), grad(v)) - inner(p, div(v)) - inner(q, div(u)))*dx
+    L = inner(f, v)*dx
 
-colors = tripcolor(p, axes = ax[1])
-fig.colorbar(colors)
-ax[1].set_title("Pressure field")
+    # the BC on the velocity space (first subspace of Z) is v = (0,0) on all boundaries
+    bcs = [DirichletBC(Z.sub(0), Constant((0, 0)), (1, 2, 3, 4))]
 
-plt.savefig("mixed_stokes_output.png")
+    # The object where we'll store the solution (u,p)
+    up = Function(Z)
+
+    # Define the nullspace of the pressure space to make solution unique
+    nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True)])
+
+    # Solve the system
+    solve(a == L, up, bcs=bcs, nullspace=nullspace)
+    u, p = up.subfunctions
+
+    # Plot the numerical and true results
+    plot_and_save(u, p, f"stokes_figs/numerical_{N=}.png")
+    plot_and_save(u_true, p_true, f"stokes_figs/exact_{N=}.png")
+
+    print("Velocity error = ", errornorm(u_true, u))
+    print("Pressure error = ", errornorm(p_true, p))
+
+if __name__ == "__main__":
+    if not os.path.exists("./stokes_figs"):
+        os.mkdir("./stokes_figs")
+
+    for k in range(1,5):
+        define_and_solve(20*k)
