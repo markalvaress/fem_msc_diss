bdb7c4fec266af73c52faa9e4ad0b65164e51718
Mark Alvares <mark.alvarez2@gmail.com>
2025-07-23 16:20:21 +0100

    added options for 1d and 2d simulations of heat eqn

--- heat_eqn_fd.py
+++ heat_eqn_fd.py
@@ -1,27 +1,40 @@
 import matplotlib.pyplot as plt
-from firedrake import quiver, UnitSquareMesh, SpatialCoordinate, VectorFunctionSpace, FunctionSpace, Function, TrialFunction, TestFunction, as_vector, inner, div, dx, DirichletBC, Constant, MixedVectorSpaceBasis, VectorSpaceBasis, solve, errornorm, pi, sin, cos, grad
+from firedrake import quiver, UnitSquareMesh, IntervalMesh, SpatialCoordinate, VectorFunctionSpace, FunctionSpace, Function, TrialFunction, TestFunction, as_vector, inner, div, dx, DirichletBC, Constant, MixedVectorSpaceBasis, VectorSpaceBasis, solve, errornorm, pi, sin, cos, grad
 from firedrake.pyplot import tripcolor
 from firedrake.pyplot.mpl import plot
 from firedrake.petsc import PETSc
+from firedrake.assemble import assemble
 import os
 import sys
 from pyop2.mpi import COMM_WORLD
 import numpy as np
 from datetime import datetime
 import os
+import utils
 
-n = 20
-dt = 1.0/(n**2)
-T = 10.0
+n = 10
+dt = 1.0/(n**4)
+T = 0.5
+DIM = 2
+save_every = 100
 
-mesh = UnitSquareMesh(n, n)
+if DIM == 1:
+    mesh = IntervalMesh(n, 1)
+    x = SpatialCoordinate(mesh)
+    ic_fn = cos(pi*x[0]) + 1
+elif DIM == 2:
+    mesh = UnitSquareMesh(n, n)
+    x, y = SpatialCoordinate(mesh)
+    ic_fn = cos(pi*x)*cos(pi*y) + 1
+else:
+    raise ValueError("DIM must be 1 or 2")
+
 V = FunctionSpace(mesh, 'CG', 1)
 u = Function(V)
 u_ = Function(V)
 v = TestFunction(V)
 
-x, y = SpatialCoordinate(mesh)
-ic = Function(V).interpolate(cos(pi*x)*cos(pi*y) + 1)
+ic = Function(V).interpolate(ic_fn)
 
 # We're using a backward Euler scheme. 
 # set the initial condition as the starting value for u and the guess for the next u
@@ -32,23 +45,35 @@ f = Function(V).interpolate(Constant(0.0))
 
 a = (inner((u - u_)/dt, v) + inner(grad(u), grad(v)))*dx
 
-
+dt_now = utils.dt_now()
 out_folder = "heat_figs/" + dt_now
 os.mkdir(out_folder)
 
 def save_frame(u, t):
     fig, ax = plt.subplots()
-    tripcolor(u, axes = ax)
+    if DIM == 1:
+        plot(u, axes = ax)
+        ax.set_ylim(0,2)
+    elif DIM == 2:
+        color_plot = tripcolor(u, axes = ax, vmin = 0, vmax = 2)
+        fig.colorbar(color_plot, ax=ax)
     fig.savefig(f"{out_folder}/heat_{t:.02f}.png")
     plt.close()
 
+E_form = inner(u, 1.0)*dx
+Es = []
 t = 0.0
-i=0
+i = 0
 while (t <= T):
     solve(a==0, u)
     u_.assign(u)
     t += dt
-    if i % 100 == 0:
+    if i % save_every == 0:
         save_frame(u,t)
 
+    E = float(assemble(E_form))
+    Es.append(E)
     i+=1
+
+with open(f"{out_folder}/energy.txt", "w") as f:
+    f.write(str(Es))
