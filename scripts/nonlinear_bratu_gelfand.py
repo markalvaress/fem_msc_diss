c9389262243f6b9aff9e2f1f5655db04653a55fd
Mark Alvares <mark.alvarez2@gmail.com>
2025-08-13 15:41:08 +0100

    made bratu gelfand sim more customisable. Added better plots

--- scripts/nonlinear_bratu_gelfand.py
+++ scripts/nonlinear_bratu_gelfand.py
@@ -7,13 +7,20 @@ import matplotlib.pyplot as plt
 import utils
 import scienceplots
 import matplotlib
+from argparse import ArgumentParser
+from num2words import num2words
 matplotlib.use('Agg')
 plt.style.use("science")
 
-out_folder = utils.init_outfolder("bratu_figs")
+parser = ArgumentParser()
+parser.add_argument("converge_to", help = "Converge to `lower` or `upper` solution.", type = str)
+parser.add_argument("n_iters", help = "Number of iterations", type = int)
+args = parser.parse_args()
+
+time_now = utils.dt_now()
+out_folder = utils.init_outfolder("bratu_figs/" + time_now)
 
 lmbda = 2.0
-n_iters = 10
 
 mesh = UnitIntervalMesh(200)
 x = SpatialCoordinate(mesh)
@@ -25,24 +32,33 @@ v = TestFunction(V)
 bc = DirichletBC(V, Constant(0), sub_domain = "on_boundary")
 
 # our initial guess
-u = Function(V).interpolate(Constant(0))
+if args.converge_to == "lower":
+    u = Function(V).interpolate(Constant(0))
+    alpha = 0.5894
+elif args.converge_to == "upper":
+    u = Function(V).interpolate(4*sin(pi*x[0]))
+    alpha = 2.1268
+else:
+    raise ValueError("converge_to must be 'lower' or 'upper'.")
+
+u_true = Function(V).interpolate(2*ln(cosh(alpha)/cosh(alpha*(1-2*x[0]))))
 
 # where we will store the solution du
 du_update = Function(V)
 
-def save_plot(u, i):
-    fig, ax = plt.subplots()
-    plot(u, axes = ax)
-    fig.savefig(f"{out_folder}/bratu_{i}.png", dpi=300)
-    plt.close()
-
 # Iterates over linear approximations of problem to find solution. Save a plot of each u_n.
-for i in range(n_iters):
-    save_plot(u,i)
+fig, ax = plt.subplots(figsize = (8,6))
+
+for i in range(args.n_iters):
+    plot(u, axes = ax, label = f"{num2words(i, to = "ordinal_num")} approximation")
     a = -inner(grad(du), grad(v))*dx + inner(lmbda*exp(u)*du, v)*dx
     F = inner(grad(u), grad(v))*dx - inner(lmbda*exp(u), v)*dx
 
     solve(a == F, du_update, bcs = bc)
     u = Function(V).interpolate(u + du_update)
 
+plot(u_true, axes = ax, label = "True solution")
+ax.legend()
+# get some axis labels up in this BLEEP
+fig.savefig(f"{out_folder}/bratu.png", dpi=300)
 utils.done(out_folder)
