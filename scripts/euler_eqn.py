3e9ff3f638c1f72e5bbafb466eb4f8d40a8c405b
Mark Alvares <mark.alvarez2@gmail.com>
2025-08-13 15:39:35 +0100

    added progress bar to euler eqn simulation and cleaned up a bit

--- scripts/euler_eqn.py
+++ scripts/euler_eqn.py
@@ -1,5 +1,4 @@
 # EULER EQUATION USING MIXED FORMULATION
-# IT DIVERGES BECAUSE OF NONLINEARITY
 
 import matplotlib.pyplot as plt
 from firedrake import *
@@ -10,6 +9,7 @@ from firedrake.assemble import assemble
 from pyop2.mpi import COMM_WORLD
 import numpy as np
 from datetime import datetime
+from tqdm import tqdm
 import utils
 import scienceplots
 import matplotlib
@@ -19,7 +19,7 @@ plt.style.use("science")
 n = 10
 dt = 1.0/(n**4)
 T = 0.5
-save_every = 100
+save_every = np.inf
 
 mesh = UnitSquareMesh(n, n)
 x, y = SpatialCoordinate(mesh)
@@ -34,11 +34,7 @@ up = Function(Z)
 up_ = Function(Z)
 v,q = TestFunctions(Z)
 
-# same as true solution of Stokes fn
-# ic = Function(V).interpolate(as_vector([
-#     1000*x**2*(1-x)**4*y**2*(1-y)*(3-5*y),
-#     -2000*x*(1-x)**3*(1-3*x)*y**3*(1-y)**2
-# ]))
+# This satisfies bcs
 ic = Function(V).interpolate(as_vector([
     -2*pi*sin(pi * x)**2 *sin(pi*y) * cos(pi * y),
     2*pi*sin(pi * y)**2 * sin(pi * x) * cos(pi * x)
@@ -47,14 +43,13 @@ ic = Function(V).interpolate(as_vector([
 # We're using a backward Euler scheme. 
 # set the initial condition as the starting value for u
 up_.sub(0).assign(ic)
-#u.assign(ic)
 
 # This would represent a force. It's 0 now.
 f = Function(V).interpolate(as_vector([Constant(0.0), Constant(0.0)]))
 
 u, p = split(up)
 u_, p_ = split(up_)
-a = (
+F = (
     inner((u - u_)/dt, v) 
     + inner(dot(u, nabla_grad(u)), v)
     - inner(p, div(v))
@@ -71,26 +66,36 @@ def save_frame(u, t):
     fig.savefig(f"{out_folder}/vel_{t:.02f}.png", dpi=300)
     plt.close()
 
+# dirichlet 0 bcs (stronger than no thru condition)
+bcs = [DirichletBC(Z.sub(0), Constant((0, 0)), (1, 2, 3, 4))]
 # Define the nullspace of the pressure space to make solution unique
 nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True, comm = COMM_WORLD)])
 
-# E_form = 0.5*inner(u, u)*dx
-# Es = []
+E_form = 0.5*inner(u, u)*dx
+Es = []
 t = 0.0
 i = 0
-while (t <= T):
-    solve(a == 0, up, nullspace = nullspace)
-    up_.assign(up)
-    t += dt
-    if i % save_every == 0:
-        u, _ = up.subfunctions
-        save_frame(u,t)
+with tqdm(total = T) as pbar:
+    while (t <= T):
+        solve(F == 0, up, bcs = bcs, nullspace = nullspace)
+        up_.assign(up)
+        t += dt
+        if i % save_every == 0:
+            u, _ = up.subfunctions
+            save_frame(u,t)
 
-    # E = float(assemble(E_form))
-    # Es.append(E)
-    i+=1
+        E = float(assemble(E_form))
+        Es.append(E)
+        i+=1
+        pbar.update(dt)
 
-# with open(f"{out_folder}/energy.txt", "w") as f:
-#     f.write(str(Es))
+with open(f"{out_folder}/energy.txt", "w") as f:
+    f.write(str(Es))
+
+i_list = list(range(len(Es)))
+plt.plot(i_list, Es)
+plt.xlabel("$i$")
+plt.ylabel(r"$E(i \Delta t)$")
+plt.savefig("energy.png", dpi=300)
 
 utils.done(out_folder)
